cef494fd723e13cd8a0d822f95b0bebd9c7a08f2
frozendolphin <122843077+frozendolphin@users.noreply.github.com>
2025-07-20 13:20:49 -0700

    used sqlc to make queries using go and made regiter command

---
+++ handler_user.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/frozendolphin/Gator/internal/database"
+	"github.com/google/uuid"
+)
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.arguments) == 0 {
+		return errors.New("no arguments were given, please enter username")
+	}
+	if len(cmd.arguments) > 1 {
+		return errors.New("too many arguments")
+	}
+
+	_, err := s.db.GetUser(context.Background(), cmd.arguments[0])
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
+	}
+
+	err = s.the_state.SetUser(cmd.arguments[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Username has been set as", cmd.arguments[0])
+	
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.arguments) == 0 {
+		return errors.New("no arguments were given, please enter username")
+	}
+	if len(cmd.arguments) > 1 {
+		return errors.New("too many arguments")
+	}
+	new_user := database.CreateUserParams {
+		ID: uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name: cmd.arguments[0],
+	}
+
+	_, ok := s.db.GetUser(context.Background(), cmd.arguments[0])
+	if ok == nil {
+		return errors.New("user already exist")
+	}
+	
+	user, err := s.db.CreateUser(context.Background(), new_user)
+	if err != nil {
+		return err
+	}
+
+	err = s.the_state.SetUser(cmd.arguments[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("User: %v was created at %v\n", user.Name, user.CreatedAt)
+	
+	return nil
+}
--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/frozendolphin/Gator/internal/config"
+	"github.com/frozendolphin/Gator/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db *database.Queries
 	the_state *config.Config
 }
 
@@ -17,13 +22,22 @@ func main() {
 		log.Fatalf("error occured: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.UrlDb)
+	if err != nil {
+		log.Fatalf("error occured opening database: %v", err)
+	}
+
+	dbQueries := database.New(db)
+
 	cfg_state := state {
+		db: dbQueries,
 		the_state: &cfg,
 	}
 
 	handler := GetCommands()
 
 	handler.register("login", handlerLogin)
+	handler.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		log.Fatal("command name required")
